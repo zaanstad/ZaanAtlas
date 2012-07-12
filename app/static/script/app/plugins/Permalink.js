12d83365b840b3b3afc616679a4001284c2a2259
Bart Baas <bartbaas@gmail.com>
2012-07-12 17:51:19 +0200

    Created a much clearer way to compose a configuration string

--- app/static/script/app/plugins/Permalink.js
+++ app/static/script/app/plugins/Permalink.js
@@ -36,7 +36,7 @@ gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
 * ``String``
 * Text for add action tooltip (i18n).
 */
-    addActionTip: "Sla het huidige kaartbeeld op als een bladwijzer",
+    addActionTip: "Bewaar dit kaartbeeld als een bladwijzer",
 
 /** api: config[availableLayersText]
 * ``String``
@@ -91,16 +91,15 @@ gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
 * Shows the window with a capabilities grid.
 */
     showCapabilitiesGrid: function() {        
-        this.initCapGrid();
+        this.initPermaDialog();
     },
 
     /**
 * private: method[initCapGrid]
 * Constructs a window with a capabilities grid.
 */
-    initCapGrid: function() {
-        this.baseUrl =  window.location.host;
-        this.baseUrl = "";
+    initPermaDialog: function() {
+    	target = this.target;
 	
 		function email(url) {
 			var emailtxt = "%0AEr is een link aangemaakt naar een kaartbeeld uit de ZaanAtlas.%0A";
@@ -125,94 +124,24 @@ gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
                  }
              });
             };
-        
-        function getElementsByTag(doc, url, tag) {
-            var urlArray = url.split( "/" );
-            var ns = urlArray[urlArray.length-1];
-            var value = doc.getElementsByTagName(ns + ":" +  tag);
-            if(!value || value == null || value.length == 0){
-                value = doc.getElementsByTagNameNS(url,  tag);
-            }
-            return value;
-        };
-		
-		// create permalink provider
-		//var permalinkProvider = new GeoExt.state.PermalinkProvider({encodeType:true});
-		var map1 = this.target.mapPanel.map;
-		//var query = "q=%7B%22map%22%3A%7B%22center%22%3A%5B" + map1.center.lon + "%2C" + map1.center.lat +
-		//			"%5D%2C%22projection%22%3A%22" + map1.projection + "%22%2C%22zoom%22%3A" + map1.zoom +
-		//			"%2C%22layers%22%3A%5B";
-		
-		var query2 = 'q={"map":{"center":[' + map1.center.lon + ',' +  map1.center.lat + '], "projection":"' +  
-					map1.projection + '"' + ',"zoom":' +  map1.zoom + ', "layers":[';
-		
-		var maplayers = this.target.mapPanel.layers.data.items;
-		
-		for (var i = 0, len = maplayers.length; i < len; i++){
-			if ((maplayers[i].data.title != "featuremanager") && (maplayers[i].data.title != "Info") && (maplayers[i].data.title != "Adres")) { 
-				if (maplayers[i].data.group == "background") {
-			
-				/**
-					query = query + "%7B%22projection%22%3A%22"+ maplayers[i].data.layer.projection.projCode + "%22%2C%22group%22%3A%22background" +
-									"%22%2C%22source%22%3A%22" + maplayers[i].data.source + "%22%2C%22title%22%3A%22" +
-									maplayers[i].data.title + "%22%2C%22name%22%3A%22" + maplayers[i].data.name +
-									"%22%2C%22visibility%22%3A"+ maplayers[i].data.layer.visibility + 
-									"%2C%22format%22%3A%22image%2Fpng%22%2C%22transparent%22%3Atrue%2C" +
-									"%22opacity%22%3A" + maplayers[i].data.layer.opacity + 
-									"%2C%22type%22%3A%22" + maplayers[i].data.type + "%22%7D%2C";					
-				
-				} else {
-
-			
-					query = query + "%7B%22projection%22%3A%22"+ maplayers[i].data.layer.projection.projCode + "%22%2C" +
-									"%22source%22%3A%22" + maplayers[i].data.source + "%22%2C%22title%22%3A%22" +
-									maplayers[i].data.title + "%22%2C%22name%22%3A%22" + maplayers[i].data.name +
-									"%22%2C%22visibility%22%3A"+ maplayers[i].data.layer.visibility + 
-									"%2C%22format%22%3A%22image%2Fpng%22%2C%22transparent%22%3Atrue%2C" +
-									"%22opacity%22%3A" + maplayers[i].data.layer.opacity + 	
-									"%2C%22type%22%3A%22" + maplayers[i].data.type + "%22%7D%2C";
-									
-				*/					
-									
-				query2 = query2 + "{\"projection\":\""+ maplayers[i].data.layer.projection.projCode + 
-									"\",\"group\":\"background\"" +
-									",\"source\":\"" + maplayers[i].data.source + 
-									"\",\"title\":\"" + maplayers[i].data.title + 
-									"\",\"name\":\"" + maplayers[i].data.name +
-									"\",\"visibility\":"+ maplayers[i].data.layer.visibility + 
-									",\"type\":\"" + maplayers[i].data.type + 
-									"\"},";	
-
-				} else {
-
-					query2 = query2 + "{\"projection\":\""+ maplayers[i].data.layer.projection.projCode + 
-									"\",\"source\":\"" + maplayers[i].data.source + 
-									"\",\"title\":\"" + maplayers[i].data.title + 
-									"\",\"name\":\"" + maplayers[i].data.name +
-									"\",\"visibility\":"+ maplayers[i].data.layer.visibility + 
-									",\"format\":\"image/png\"" +
-									",\"transparent\":true" +
-									",\"opacity\":" + maplayers[i].data.layer.opacity + 
-									",\"type\":\"" + maplayers[i].data.type + 
-									"\"},";					
-		
-				};
-			};
-		};
-		
-		query2 = query2.substring (0, query2.length-1);
-		query2 = query2 + "]}}";
-		window.location.hash = query2;
+            
 		var lyrs = "";
-		for (var i = 0, len = map1.layers.length; i < len; i++){
-			lyrs = map1.layers[i].name + "%0A" + lyrs;
+		var configObj = {};
+		configObj.map = target.getState().map;
+		delete configObj.map.maxExtent;
+		
+		// remove unnecessary information
+		for (var i = 0, len = configObj.map.layers.length; i < len; i++){
+			delete configObj.map.layers[i].capability;
+			delete configObj.map.layers[i].selected;
+			delete configObj.map.layers[i].format;
+			delete configObj.map.layers[i].fixed;
+			// generate list of layernames
+			lyrs = configObj.map.layers[i].name + "%0A" + lyrs;
 		}
-		//var str_provider = permalinkProvider.getLink();
-		//x = str_provider.indexOf("?");
-		//if (x != -1) {	
-		//	str_provider = str_provider.substring(0, x);		
-		//};
-		//URL = str_provider + query2;
+		var configStr = Ext.util.JSON.encode(configObj);
+		
+		window.location.hash = 'q=' + configStr; //query2;
 	
         var centre = [{
             xtype: 'container',
