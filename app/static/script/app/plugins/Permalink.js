0ee3cb1234d39e941c5067f6706a2d9d53297340
Bart Baas <b.baas@zaanstad.nl>
2013-02-20 17:08:42 +0100

    Async permalink tool and tiny url possibility

--- app/static/script/app/plugins/Permalink.js
+++ app/static/script/app/plugins/Permalink.js
@@ -16,68 +16,79 @@
  */
 Ext.namespace("gxp.plugins");
 
-/** api: constructor
-* .. class:: Permalink(config)
-*
-* Plugin for removing a selected layer from the map.
-*/
-gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
-    
-/** api: ptype = gxp_permalink */
+    /** api: constructor
+    * .. class:: Permalink(config)
+    *
+    * Plugin for removing a selected layer from the map.
+    */
+    gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
+        
+    /** api: ptype = gxp_permalink */
     ptype: "app_permalink",
-    
-/** api: config[addActionMenuText]
-* ``String``
-* Text for add menu item (i18n).
-*/
+        
+    /** api: config[addActionMenuText]
+    * ``String``
+    * Text for add menu item (i18n).
+    */
     addActionMenuText: "Bladwijzer",
 
-/** api: config[addActionTip]
-* ``String``
-* Text for add action tooltip (i18n).
-*/
+    /** api: config[addActionTip]
+    * ``String``
+    * Text for add action tooltip (i18n).
+    */
     addActionTip: "Bewaar dit kaartbeeld als een bladwijzer",
 
-/** api: config[availableLayersText]
-* ``String``
-* Text for the available layers (i18n).
-*/
+    /** api: config[availableLayersText]
+    * ``String``
+    * Text for the available layers (i18n).
+    */
     availableLayersText: "Permalink",
-    
-/** api: config[titleText]
-* ``String``
-* Text for the layer title (i18n).
-*/
+        
+    /** api: config[titleText]
+    * ``String``
+    * Text for the layer title (i18n).
+    */
     titleText: "Maak een bladwijzer",
-    
-/** api: config[doneText]
-* ``String``
-* Text for Done button (i18n).
-*/
+        
+    /** api: config[doneText]
+    * ``String``
+    * Text for Done button (i18n).
+    */
     doneText: "Sluiten",
-    
-/** api: config[menuText]
-* ``String``
-* Text for upload button (only renders if ``upload`` is provided).
-*/
+        
+    /** api: config[menuText]
+    * ``String``
+    * Text for upload button (only renders if ``upload`` is provided).
+    */
     menuText: "Bladwijzer",
 
-/** private: method[constructor]
- */
+    /** api: config[troubleText]
+    * ``String``
+    * Text for Done button (i18n).
+    */
+    emailBodyText: null,
+
+    APIS: {
+        tinyurl: "http://tinyurl.com/api-create.php?url=",
+        bitly: "http://api.bitly.com/v3/shorten?"
+        },
+
+    /** private: method[constructor]
+     */
     constructor: function(config) {
         gxp.plugins.Permalink.superclass.constructor.apply(this, arguments);
     },
-    
-/** api: method[addActions]
- */
+        
+    /** api: method[addActions]
+     */
     addActions: function() {
         var selectedLayer;
         var actions = gxp.plugins.Permalink.superclass.addActions.apply(this, [{
             tooltip : this.addActionTip,
             menuText: this.addActionMenuText,
             text: this.menuText,
-            icon: "../theme/app/img/permalink.png",
-            handler : this.showCapabilitiesGrid,
+            iconCls:"icon-permalink",
+            handler : this.showDialog,
             scope: this
         }]);
         
@@ -86,154 +97,258 @@ gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
         });
         return actions;
     },
-        
-/** api: method[showCapabilitiesGrid]
-* Shows the window with a capabilities grid.
-*/
-    showCapabilitiesGrid: function() {        
-        this.initPermaDialog();
+
+    /** private: method[getShortURL]
+     *
+     * Saves the map config and displays the URL in a window.
+     */ 
+    getShortURL: function(callback, scope, longURL) {
+        //alert(longURL.toString().length);
+        if (longURL.toString().length > 1536) {
+            this.getTinyURL(callback, scope, longURL);
+        } else {
+            this.getBitlyURL(callback, scope, longURL);
+        }
     },
 
-    /**
-* private: method[initCapGrid]
-* Constructs a window with a capabilities grid.
-*/
-    initPermaDialog: function() {
-    	target = this.target;
-	
-		function email(url) {
-			var emailtxt = "%0AEr is een link aangemaakt naar een kaartbeeld uit de ZaanAtlas.%0A";
-			var api_key = "R_b3889752450cd7113ec5a5c06eca8b07";
-			var api_login = "teamgeo";
-			var api_url = "api.bitly.com";
-            var request = "longUrl="+encodeURIComponent(url)+"&login="+api_login+"&apiKey="+api_key+"&callback=?";
-            var OLrequest = OpenLayers.Request.POST({
-                 url : "http://"+api_url+"/v3/shorten?",
-                 async: true,
-                 data : request,
-                 headers: {
-                     "Content-Type": "text/javascript"
-                 },
-                 success : function(response) {
-                     //alert(response.responseText);
-                     var json = eval("(" + response.responseText.slice(2, -2) + ")");
-                     window.location = "mailto:?SUBJECT=ZaanAtlas%20link&BODY="+emailtxt+"%0AVerkorte link:%20"+json.data.url
-                     				   +"%0A%0ADe volgende kaartlagen zijn actief:%0A"+lyrs
-                     				   +"%0AOndergronden:%0A"+bglyrs;
-                 },
-                 failure : function(response) {
-                 	alert(response.responseText);
-                 }
-             });
-            };
+    /** private: method[getTinyURL]
+     *
+     * Saves the map config and displays the URL in a window.
+     */ 
+    getTinyURL: function(callback, scope, longURL) {
+        OpenLayers.Request.GET({
+            url: this.APIS.tinyurl + encodeURIComponent(longURL),
+            async: true,
+            headers: {
+             "Content-Type": "application/html"
+            },
+            callback: function(response) {
+                if (callback) {
+                    callback.call(scope || this, response.responseText);
+                }
+            },
+            failure : function(response) {
+                this.displayTrouble(response.responseText);
+            },
+            scope: this
+        });
+    },
+
+    /** private: method[getBitlyURL]
+     *
+     * Saves the map config and displays the URL in a window.
+     */ 
+    getBitlyURL: function(callback, scope, longURL) {
+        var api_key = "R_b3889752450cd7113ec5a5c06eca8b07";
+        var api_login = "teamgeo";
+        var request = "longUrl="+encodeURIComponent(longURL)+"&login="+api_login+"&apiKey="+api_key+"&callback=?";
+
+        OLrequest = OpenLayers.Request.POST({
+            url: this.APIS.bitly,
+            async: true,
+            data: request,
+            headers: {
+             "Content-Type": "text/javascript"
+            },
+            callback: function(response) {
+                if (callback) {
+                    var json = eval("(" + response.responseText.slice(2, -2) + ")");
+                    callback.call(scope || this, json.data.url);
+                }
+            },
+            failure : function(response) {
+                this.displayTrouble(response.responseText);
+            },
+            scope: this
+         });
+    },
+
+    displayTrouble: function(msg, status) {
+        Ext.Msg.show({
+            title: this.troubleText,
+            msg: (msg || "Probleem bij het aanmaken van de link"),
+            icon: Ext.MessageBox.WARNING
+        });
         
-		function popupLink(url) {
-            //alert(encodeURIComponent(url));
-			var api_key = "R_b3889752450cd7113ec5a5c06eca8b07";
-			var api_login = "teamgeo";
-			var api_url = "api.bitly.com";
-            var request = "longUrl="+encodeURIComponent(url)+"&login="+api_login+"&apiKey="+api_key+"&callback=?";
-            var OLrequest = OpenLayers.Request.POST({
-                 url : "http://"+api_url+"/v3/shorten?",
-                 async: true,
-                 data : request,
-                 headers: {
-                     "Content-Type": "text/javascript"
-                 },
-                 success : function(response) {
-                     var json = eval("(" + response.responseText.slice(2, -2) + ")");
-                     Ext.Msg.show({
-						title: "Verkorte link",
-						msg: json.data.url,
-						icon: Ext.MessageBox.INFO
-					});
-                 },
-                 failure : function(response) {
-                 	alert(response.responseText);
-                 }
-             });
-            };
+    },
+
+    /** private: method[showUrl]
+     */
+    showUrl: function(shortUrl) {
+        var win = new Ext.Window({
+            title: this.menuText,
+            layout: 'form',
+            labelAlign: 'top',
+            modal: true,
+            bodyStyle: "padding: 2px",
+            width: 250,
+            items: [{
+                xtype: 'textfield',
+                fieldLabel: "Link",
+                readOnly: true,
+                anchor: "100%",
+                selectOnFocus: true,
+                value: shortUrl
+            }]
+        });
+        win.show();
+    },
+
+    /** private: method[showMail]
+     */
+    showMail: function(shortUrl) {
+        var emailtxt = "%0AEr is een link aangemaakt naar een kaartbeeld uit de ZaanAtlas.%0A";
+        window.location = "mailto:?SUBJECT=ZaanAtlas%20link&BODY="+emailtxt
+                        +"%0AVerkorte link:%20"+shortUrl
+                        + this.emailBodyText;
+    },
             
-		var lyrs = "";
-		var bglyrs= "";
-		var configObj = {};
-		configObj.map = target.getState().map;
-		//delete configObj.map.maxExtent;
-		
-		for (var i = 0, len = configObj.map.layers.length; i < len; i++){
-		
-		// remove unnecessary information
-			delete configObj.map.layers[i].capability;
-			delete configObj.map.layers[i].selected;
-			delete configObj.map.layers[i].format;
-			delete configObj.map.layers[i].fixed;
-            delete configObj.map.layers[i].minscale;
-            delete configObj.map.layers[i].maxscale;
-            if (configObj.map.layers[i].styles == "") {
-                delete configObj.map.layers[i].group;
+    /** api: method[showDialog]
+    * Shows the window with a capabilities grid.
+    */
+    showDialog: function() {        
+        target = this.target;
+        configObj = {};
+        configObj.map = target.getState().map;
+        var configString = createUrlString();
+        this.emailBodyText = generateMailBody();
+
+        function createUrlString() {
+            //delete configObj.map.maxExtent;
+            
+            for (var i = 0, len = configObj.map.layers.length; i < len; i++){
+            
+            // remove unnecessary information
+                delete configObj.map.layers[i].capability;
+                delete configObj.map.layers[i].selected;
+                delete configObj.map.layers[i].format;
+                delete configObj.map.layers[i].fixed;
+                delete configObj.map.layers[i].minscale;
+                delete configObj.map.layers[i].maxscale;
+                if (configObj.map.layers[i].styles == "") {
+                    delete configObj.map.layers[i].group;
+                }
+                if (configObj.map.layers[i].cql_filter == "") {
+                    delete configObj.map.layers[i].cql_filter;
+                }
+                if (configObj.map.layers[i].group == "") {
+                    delete configObj.map.layers[i].group;
+                }
+                if (configObj.map.layers[i].tiled == true) {
+                    delete configObj.map.layers[i].tiled;
+                }
+                if (configObj.map.layers[i].cached == false) {
+                    delete configObj.map.layers[i].cached;
+                }
             }
-            if (configObj.map.layers[i].cql_filter == "") {
-                delete configObj.map.layers[i].cql_filter;
+            return Ext.util.JSON.encode(configObj);
+        };
+
+        function generateMailBody() {
+            lyrs = "";
+            bglyrs= "";
+            
+            for (var i = 0, len = configObj.map.layers.length; i < len; i++){
+            // generate list of layernames
+                if (configObj.map.layers[i].group == "background") {
+                    bglyrs = configObj.map.layers[i].title + "%0A" + bglyrs;
+                } else {
+                    lyrs = configObj.map.layers[i].title + "%0A" + lyrs;
+                }
             }
-			if (configObj.map.layers[i].group == "") {
-				delete configObj.map.layers[i].group;
-			}
-			if (configObj.map.layers[i].tiled == true) {
-				delete configObj.map.layers[i].tiled;
-			}
-            if (configObj.map.layers[i].cached == false) {
-                delete configObj.map.layers[i].cached;
+            return "%0A%0ADe volgende kaartlagen zijn actief:%0A"+lyrs
+                    +"%0AOndergronden:%0A"+bglyrs;
+        };
+
+        function hideLinkButton(config) {
+            /** This cookie's size increases, and oversize Jetty's memory for cookies (4k by default) 
+             * Increasing this dedicated memory to 16k resolves the pb (adding a tag 
+             * <headerBufferSize>16384</headerBufferSize> in web/pom.xml) 
+             */
+            if (getWindowLocation(config).toString().length > 2356) {
+                return true;
             }
-			
-		// generate list of layernames
-			if (configObj.map.layers[i].group == "background") {
-				bglyrs = configObj.map.layers[i].title + "%0A" + bglyrs;
-			} else {
-				lyrs = configObj.map.layers[i].title + "%0A" + lyrs;
-			}
-		}
-		var configStr = Ext.util.JSON.encode(configObj);
-		
-		window.location.hash = 'q=' + configStr; //query2;
-	
+            return false;
+        };
+
+        function hideLongLinkMsg(config) {
+            if (getWindowLocation(config).toString().length > 2048) {
+                return false;
+            }
+            return true;
+        };
+
+        function getWindowLocation(config) {
+            pageAddress = window.location.href.split("#")[0];
+            return pageAddress + '#q=' + config;
+        };
+
+        function setWindowLocation(config) {
+            window.location.hash = 'q=' + config;
+        };
+    
         var centre = [{
-            xtype: 'container',
-            layout: 'anchor',
+            xtype: "container",
+            layout: "anchor",
             style: {
-                padding: '10px'
+                padding: "10px"
             },
             items: [
-			{
-				xtype: "container",
-				region: "center",
-				html: 	"<b>Bladwijzer of Favorieten</b><br>" +
-						"<p>Met behulp van een bladwijzer is het mogelijk om een kaartbeeld " + 
-						"te bewaren om deze de volgende keer eenvoudig en snel terug te kunnen vinden. " + 
-						"Gebruik hiervoor de bladwijzer of bookmark functie van de webbrowser terwijl dit venster zichtbaar is.<br><br><br>" +
-						"<b>Email</b><br>" +
-						"<p>Het is mogelijk om een verkorte link te versturen voor dit kaartbeeld naar een emailadres.<br><br></p>",
-				scope: this
-        	}, {
-                xtype: 'button',
-                iconCls:'icon-email-go',
-                text: 'Email de link voor deze kaart',
+            {
+                xtype:  "container",
+                region: "center",
+                html:   "<font color='red'>Let op: de bladwijzer die je probeert aan te maken bevat erg veel informatie en kan mogelijk problemen gegeven in sommige oudere browsers.<br><br>",
+                hidden: hideLongLinkMsg(configString),
+                scope: this
+            }, {
+                xtype:  "container",
+                region: "center",
+                html:   "<b>Bladwijzer of Favoriet</b><br>" +
+                        "<p>Met behulp van een bladwijzer is het mogelijk om een kaartbeeld " + 
+                        "te bewaren om deze de volgende keer eenvoudig en snel terug te kunnen vinden. " + 
+                        "Gebruik hiervoor de bladwijzer of bookmark functie van de webbrowser terwijl dit venster zichtbaar is.<br><br></p>"
+            }, {
+                xtype:  "button",
+                iconCls:"icon-world-link",
+                text:   "Open de bladwijzerlink",
                 handler: function() {
-                    email(window.location);
+                    this.showUrl(getWindowLocation(configString));
                 },
                 scope: this
             }, {
-				xtype: "container",
-				region: "center",
-				html: 	"<br><br><b>Verkorte link</b><br>" +
-						"<p>Lange links zijn onhandig om door te sturen of te plaatsen op sociale netwerken. Maak daarom een " +
-						"verkorte link aan via onderstaande knop (bit.ly), de korte link verwijst naar dit kaarbeeld.<br><br></p>",
-				scope: this
-        	}, {
-                xtype: 'button',
-                iconCls:'icon-link',
-                text: 'Verkorte link aanmaken',
+                xtype:  "container",
+                region: "center",
+                hidden: hideLinkButton(configString),
+                html:   "<br><br><b>Email</b><br>" +
+                        "<p>Het is mogelijk om een verkorte link te versturen voor dit kaartbeeld naar een emailadres.<br><br></p>"
+            }, {
+                xtype:  "button",
+                iconCls:"icon-email-link",
+                text:   "Email de link voor deze kaart",
+                hidden: hideLinkButton(configString),
                 handler: function() {
-                    popupLink(window.location);
+                    this.getShortURL(this.showMail, null, getWindowLocation(configString));
+                },
+                scope: this
+            }, {
+                xtype:  "container",
+                region: "center",
+                hidden: hideLinkButton(configString),
+                html:   "<br><br><b>Verkorte link</b><br>" +
+                        "<p>Lange links zijn onhandig om door te sturen of te plaatsen op sociale netwerken. Maak daarom een " +
+                        "verkorte link aan via onderstaande knop, de korte link verwijst naar dit kaarbeeld.<br><br></p>"
+            }, {
+                xtype:  "button",
+                iconCls:"icon-hourglass-link",
+                text:   "Verkorte link aanmaken",
+                hidden: hideLinkButton(configString),
+                handler: function(f,e) {
+                    if(e.shiftKey){
+                        this.getTinyURL(this.showUrl, null, getWindowLocation(configString));
+                    }
+                    else {
+                        this.getShortURL(this.showUrl, null, getWindowLocation(configString));
+                    }
                 },
                 scope: this
             }
@@ -245,30 +360,33 @@ gxp.plugins.Permalink = Ext.extend(gxp.plugins.Tool, {
         new Ext.Button({
             text: this.doneText,
             handler: function() {
-                this.capGrid.close();
+                this.dialog.close();
             },
             scope: this
         })
-        ];	
-		
-        this.capGrid = new Ext.Window({
+        ];  
+        
+        this.dialog = new Ext.Window({
             title: this.titleText,
             closeAction: "close",
-            height: 410,
+            //height: 430,
             width: 390,
             modal: true,
             resizable: false, 
             items: centre,
             bbar: bbarItems
         });
-        
-        this.capGrid.on('close', function() {
-			 window.location.hash = "";
-		});
 
-        this.capGrid.show();
-        formulier = this.capGrid;
-        }		
+        this.dialog.on('show', function() {
+             setWindowLocation(configString);
+        });
+        
+        this.dialog.on('close', function() {
+             window.location.hash = "";
+        });
+
+        this.dialog.show();
+    }
  });		 
 
 Ext.preg(gxp.plugins.Permalink.prototype.ptype, gxp.plugins.Permalink);
